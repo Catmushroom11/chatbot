7090697d8b95910feaf71e6361e4397d8929097c
Catmushroom11 <109758632+Catmushroom11@users.noreply.github.com>
2022-07-21 13:25:57 -0500

    Create Inside Code.py

---
+++ Inside Code.py
@@ -0,0 +1,5 @@
+#chatbot introduction
+print('Hello. I am Zyxo 64. I am a chatbot.')
+print('I like animals and I love to talk about food.')
+name = input('What is you name?: ')
+print('Hello', name, ', Nice to meet you.')
